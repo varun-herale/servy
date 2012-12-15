d77e4b1000a79171bd63922c404c6f63422bd03d
Varun Herale <varun.herale@gmail.com>
2012-12-16 01:06:28 +0530

    First Draft

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
-from flask import Flask
+from flask import *
+import logging
+import logging.handlers
+import datetime
+
 app = Flask(__name__)
+LOG_FILENAME = 'req.log'
+info_log = logging.getLogger('req')
+info_log.setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    LOG_FILENAME,
+    maxBytes=1024 * 1024 * 100,
+    backupCount=20
+    )
+
+info_log.addHandler(handler)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    info_log.info('\t'.join([
+            datetime.datetime.today().ctime(),
+            request.remote_addr,
+            request.method,
+            request.url,
+            request.data]))
+    return render_template('index.html')
+  
+@app.route('/<path:url>')
+def new_path(url):
+    #info_log.info('\t'.join([request.remote_addr,request.url]))
+    info_log.info('\t'.join([
+            datetime.datetime.today().ctime(),
+            request.remote_addr,
+            request.method,
+            request.url,
+            request.data]))
+    return render_template(url)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
